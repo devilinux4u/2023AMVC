94c7183c5e0eca9519d5e8e25863f5a9008030b1
devilinux4u <solo345r@gmail.com>
2025-04-15 03:59:36 +0545

    new day

--- 2023AMVC/ChatHub/BasicChatHub.cs
+++ 2023AMVC/ChatHub/BasicChatHub.cs
@@ -1,8 +1,48 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
 
 namespace _2023AMVC.ChatHub
 {
     public class BasicChatHub : Hub
     {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        public BasicChatHub(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public string GetUserId()
+        {
+            var httpContext = Context.GetHttpContext();
+            var userId = httpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return userId;
+        }
+
+        public async Task<IList<string>> GetUserRoles(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            var roles = await _userManager.GetRolesAsync(user);
+            return roles;
+        }
+
+        public static List<string> GroupsJoined { get; set; } =new List<string>();
+
+        [Authorize]
+        public async Task JoinGroup(string sender)
+        {
+            var user = GetUserId();
+            var role = (await GetUserRoles(user)).FirstOrDefault();
+            if(!GroupsJoined.Contains(Context.ConnectionId + ":" + role))
+            {
+                GroupsJoined.Add(Context.ConnectionId + ":" + role);
+
+                await Groups.AddToGroupAsync(Context.ConnectionId, role);
+
+            }
+        }
+
     }
 }
